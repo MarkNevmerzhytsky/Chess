c0a30f16b37d05bbb15d0c481228a41c63cf1891
Artem <artemyermolenko14@gmail.com>
2024-07-04 16:59:53 +0200

    draw function

--- game/screen.py
+++ game/screen.py
@@ -7,6 +7,18 @@ class Screen:
         pygame.display.set_caption('Simulation')
         self.screen = pygame.display.set_mode((width, height))
 
+    def draw(self):
+        while True:
+            self.screen.fill((100, 100, 100))
+
+            self.screen.blit(pygame.image.load("C:/Users/Artem/PycharmProjects/python/Chess/images/bb.png"), (100, 100))
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    break
 
 
-
+screen = Screen(1000, 1000)
+screen.draw()
